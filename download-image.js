1c44a594c0ed98e38fc0d6cf662bb4ea1f225474
石松岩.前端架构组 <songyan_shi@163.com>
2023-04-11 10:21:08 +0800

    图片下载完毕

---
+++ download-image.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+const Axios = require('axios')
+const imgs = require('./imgs.json')
+
+
+async function downloadFile(url, filepath, name) {
+	if (!fs.existsSync(filepath)) {
+		fs.mkdirSync(filepath);
+	}
+	const mypath = path.resolve(filepath, name);
+	const writer = fs.createWriteStream(mypath);
+	const response = await Axios({
+		url,
+		method: "GET",
+		responseType: "stream",
+	});
+	response.data.pipe(writer);
+	return new Promise((resolve, reject) => {
+		writer.on("finish", resolve);
+		writer.on("error", reject);
+	});
+}
+
+const init = async () => {
+	for (let index = 0; index < imgs.length; index++) {
+		console.log(index)
+		if (index < 67) continue
+		const element = imgs[index];
+		const { smUrl, lgUrl, title, subtitle } = element
+		await downloadFile(smUrl, './sm-images', `${title}-${subtitle}.jpg`)
+		await downloadFile(lgUrl, './lg-images', `${title}-${subtitle}.jpg`)
+	}
+}
+
+init().catch(err => {
+	console.log('存在图片下载失败')
+})
